483f1fce43b9e83a178ee557ba9ad81835e7475c
Samuel <s-bridge@live.co.uk>
2014-10-03 08:45:59 +0100

    [Feed] - Added fallback and client ID check

--- fb-feed.php
+++ fb-feed.php
@@ -1,46 +1,49 @@
 <? // Settings
 	
 	$client = 'PropellerComms';
+	$clientID = 000;
 	$access_token = 'xxx';
 	$app_secret = 'xxx';
 
 	$count = 5;
-	$char_limit = 200;
+	$char_limit = 126;
 
-    $json = file_get_contents('https://graph.facebook.com/'.$client.'/feed?access_token='.$access_token.'|'.$app_secret);
-    $feed = json_decode($json);
+	try {
+
+    	$json = file_get_contents('https://graph.facebook.com/'.$client.'/feed?access_token='.$access_token.'|'.$app_secret);
+    	$feed = json_decode($json);
+
+	} catch(Exception $e) {};
+
 
 ?>
 
-<div class="feed-container">
-	
-	<? for($i = 0; $i < $count; $i++) :?>
+<? if(isset($json)) :?>
+
+	<? for($i = 0; $i < $count; $i++) : ?>
 
 		<? 	$entry = $feed->data[$i];
-			if (!isset($entry->message)) : $count++; continue; endif;
+
+			if (isset($entry->story) || $entry->from->id != $clientID) : $count++; continue; endif;
+
 		?>
+
 		
-		<div class="entry">
-			
+		<p>
+			<?= substr($entry->message, 0, $char_limit); ?>
+			<?= $char_limit > strlen($entry->message) ? '' : '...'; ?>
 
-			<? if(isset($entry->picture)) :?>
-				<p>
-					<img src="<?= $entry->picture;?>" alt="<?= $entry->from->name;?>" />
-				</p>
+			<? if(isset($entry->picture)) : ?>
+
+				<a href="<?= $entry->picture;?>" target="_blank">View image...</a>
+
 			<? endif;?>
-			
-			<p><?= substr($entry->message, 0, $char_limit); ?><?= $char_limit > strlen($entry->message) ? '' : '...';?></p>
+		</p>
 
-			<? if(isset($entry->link)) :?>
-				<p>
-					<a href="<?= $entry->link;?>" target="_blank">Read more...</a>
-				</p>
-			<? endif;?>
-			
-			<p>Posted on: <?= date('d/m/Y', strtotime($entry->created_time));?> at <?= date('G:i', strtotime($entry->created_time));?></p>
+	<? endfor; ?>
 
-		</div>
+<? else :?>
 
-	<? endfor;?>
-	
-</div>
+	<p>Sorry, Facebook feed not available. Try again later.</p>
+
+<? endif;?>
